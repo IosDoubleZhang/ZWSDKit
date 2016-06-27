d9c7ee66a8bdcff68259ce5a01e168e78a8adebf
ZHOUYUBIN <625887489@qq.com>
2016-06-28 02:30:38 +0800

    加入了LeanClound 写了一个基本类型 myPoint
    Signed-off-by: ZHOUYUBIN <625887489@qq.com>

---
+++ lifangti/main/Src/m/LeanCloundModel/LeanClounModelHeader.m
@@ -0,0 +1,13 @@
+//
+//  LeanClounModelHeader.m
+//  lifangti
+//
+//  Created by xthink4 on 16/6/28.
+//  Copyright © 2016年 xthink. All rights reserved.
+//
+
+#import "LeanClounModelHeader.h"
+
+@implementation LeanClounModelHeader
+
+@end
---
+++ lifangti/main/Src/m/LeanCloundModel/myPoint.m
@@ -0,0 +1,19 @@
+//
+//  myPoint.m
+//  zhaojiuwanwu
+//
+//  Created by guozhou-MAC001 on 15/7/2.
+//  Copyright (c) 2015年 guozhou-MAC001. All rights reserved.
+//
+
+#import "myPoint.h"
+
+@implementation myPoint
+
+@dynamic content,location, pub, duration,comments,img,imgs,video,videoThumbnail,zambiaArr,reward,userimg,username,dist,isCashSelected,age,sex,nickname,flag;
+
++ (NSString *)parseClassName {
+    return @"myPoint";
+}
+
+@end
