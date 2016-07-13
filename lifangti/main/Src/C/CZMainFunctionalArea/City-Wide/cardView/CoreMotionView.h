fc843f89640eae81e04ddb03afe7495f319256f5
xxzzxxzzzzs <xxzzxxzzzzs@163.com>
2016-07-13 16:26:38 +0800

    nn
    Signed-off-by: xxzzxxzzzzs <xxzzxxzzzzs@163.com>

---
+++ lifangti/main/Src/C/CZMainFunctionalArea/City-Wide/cardView/CoreMotionView.h
@@ -0,0 +1,13 @@
+//
+//  CoreMotionView.h
+//  lifangti
+//
+//  Created by xthink4 on 16/7/12.
+//  Copyright © 2016年 xthink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreMotionView : UIView
+
+@end
---
+++ lifangti/main/Src/C/CZMainFunctionalArea/City-Wide/cardView/CoreMotionView.m
@@ -0,0 +1,21 @@
+//
+//  CoreMotionView.m
+//  lifangti
+//
+//  Created by xthink4 on 16/7/12.
+//  Copyright © 2016年 xthink. All rights reserved.
+//
+
+#import "CoreMotionView.h"
+
+@implementation CoreMotionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
